c3122286f70ea9445b718814b0ec419140de9e7b
antonovmike <antonovma@outlook.com>
2024-01-17 20:25:54 +0400

    models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger
+from sqlalchemy import BigInteger, ForeignKey
 from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
@@ -17,10 +17,27 @@ class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id: Mapped[int] = mapped_column(BigInteger)
-    name: Mapped[str] = mapped_column()
-    phone: Mapped[str] = mapped_column()
-    orders: Mapped[list] = relationship('Order', back_populates='user')
+    tg_id = mapped_column(BigInteger)
 
-    def __repr__(self):
-        return f'<Eser {self.name}>'
+    # def __repr__(self):
+    #     return f'<Eser {self.name}>'
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    products = relationship('Product', back_populates='category')
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    price: Mapped[int] = mapped_column()
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+
+    category = relationship('Category', back_populates='products')
+    # orders = relationship('Order', back_populates='products')
